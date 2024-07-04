50d5301ed74955718c858e33dd1b2bcc0605aa7d
Nikita Shpulin <shpulinn@mail.ru>
2024-07-04 16:51:27 +0200

    PlayerNavMeshLinkController script created

---
+++ Assets/_Neighbours/Scripts/Player/PlayerNavMeshLinkController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace _Neighbours.Scripts.Player
+{
+    public class PlayerNavMeshLinkController : MonoBehaviour
+    {
+        private NavMeshAgent agent;
+        private Animator animator;
+
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+            agent.autoTraverseOffMeshLink = false;
+        }
+
+        void Update()
+        {
+            if (agent.isOnOffMeshLink)
+            {
+                StartCoroutine(HandleOffMeshLink());
+            }
+        }
+
+        private IEnumerator HandleOffMeshLink()
+        {
+            OffMeshLinkData data = agent.currentOffMeshLinkData;
+            Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+
+            //animator.SetTrigger("Jump");
+
+            float jumpDuration = .2f;
+            float jumpSpeed = .5f;
+
+            float time = 0.0f;
+            Vector3 startPos = transform.position;
+            var renderer = agent.GetComponentInChildren<MeshRenderer>();
+            renderer.enabled = false;
+            while (time < jumpDuration)
+            {
+                time += Time.deltaTime * jumpSpeed;
+                agent.transform.position = Vector3.Lerp(startPos, endPos, time / jumpDuration);
+                yield return null;
+            }
+
+            renderer.enabled = true;
+
+            agent.CompleteOffMeshLink();
+        }
+    }
+
+}
