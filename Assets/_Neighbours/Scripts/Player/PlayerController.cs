8c7808f904c75dd2cd81a735fe59ec364bf1df4f
Nikita Shpulin <shpulinn@mail.ru>
2024-07-05 18:16:38 +0200

    player controller updated

--- Assets/_Neighbours/Scripts/Player/PlayerController.cs
+++ Assets/_Neighbours/Scripts/Player/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private LayerMask doorLayer;
     [SerializeField] private LayerMask interactableLayer;
 
     private PlayerStateMachine _stateMachine;
@@ -27,18 +26,18 @@ public class PlayerController : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
+            Physics.Raycast(ray, out hit, 100);
             if (Physics.Raycast(ray, out hit, 100, groundLayer))
             {
                 _stateMachine.ChangeState(new MoveState(_stateMachine, hit.point));
-            }
-
-            if (Physics.Raycast(ray, out hit, 100, interactableLayer))
+            } 
+            else if (Physics.Raycast(ray, out hit, 100, interactableLayer))
             {
                 IInteractable interactable = hit.collider.GetComponent<IInteractable>();
                 if (interactable != null)
                 {
                     float distance = Vector3.Distance(transform.position, hit.point);
-                    if (distance < 1.0f) // Если достаточно близко
+                    if (distance < 2.0f) // Если достаточно близко
                     {
                         _stateMachine.ChangeState(new InteractState(_stateMachine, interactable));
                     }
@@ -49,39 +48,6 @@ public class PlayerController : MonoBehaviour
                 }
             }
         }
-    }
-
-    private void FindAndUseDoor(Vector3 destination)
-    {
-        Collider[] doors = Physics.OverlapSphere(transform.position, 10, doorLayer);
-
-        if (doors.Length > 0)
-        {
-            Collider nearestDoor = doors[0];
-            float nearestDistance = Vector3.Distance(transform.position, doors[0].transform.position);
-
-            foreach (Collider door in doors)
-            {
-                float distance = Vector3.Distance(transform.position, door.transform.position);
-                if (distance < nearestDistance)
-                {
-                    nearestDoor = door;
-                    nearestDistance = distance;
-                }
-            }
-
-            Door doorScript = nearestDoor.GetComponent<Door>();
-            doorScript.OpenDoor();
-            agent.SetDestination(nearestDoor.transform.position);
-            StartCoroutine(CloseDoorAfterUse(doorScript, destination));
-        }
-    }
-
-    private IEnumerator CloseDoorAfterUse(Door door, Vector3 destination)
-    {
-        yield return new WaitForSeconds(1);
-        door.CloseDoor();
-        agent.SetDestination(destination);
     }
 }
 
