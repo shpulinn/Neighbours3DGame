e77640a6016fb6f1017544fdbacde09c3c020046
Nikita Shpulin <shpulinn@mail.ru>
2024-07-05 18:16:54 +0200

    IInteractable updated

--- Assets/_Neighbours/Scripts/Interactables/IInteractable.cs
+++ Assets/_Neighbours/Scripts/Interactables/IInteractable.cs
@@ -1,4 +1,8 @@
 public interface IInteractable
 {
     public void Interact();
+    public void TerminateInteraction();
+    public float InteractionDuration { get; }
+
+    public float InteractionProgress { get; }
 }
