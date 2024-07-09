399e70c3003b55c45387808b0c0d0baa814ee3e6
Nikita Shpulin <shpulinn@mail.ru>
2024-07-09 16:57:01 +0200

    Inventory script created

---
+++ Assets/_Neighbours/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Neighbours.Scripts
+{
+    public class Inventory : MonoBehaviour
+    {
+        private List<Item> items = new List<Item>();
+
+        public bool HasItem(Item item)
+        {
+            return items.Contains(item);
+        }
+
+        public void AddItem(Item item)
+        {
+            if (!items.Contains(item))
+            {
+                items.Add(item);
+            }
+        }
+
+        public void RemoveItem(Item item)
+        {
+            if (items.Contains(item))
+            {
+                items.Remove(item);
+            }
+        }
+    }
+}
